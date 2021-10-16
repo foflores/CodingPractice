af1484d56db5635547d3675c89d234a7118c797d
Favian Flores <floresf1997@gmail.com>
2021-10-15 20:06:57 -0400

    daily practice

--- LeetCode/0010RegularExpressionMatching.py
+++ LeetCode/0010RegularExpressionMatching.py
@@ -1,3 +1,25 @@
 class Solution:
 	def isMatch(self, s: str, p: str) -> bool:
-		pass
+		if len(p) == 0:
+			if len(s) == 0:
+				return True
+			else:
+				return False
+
+		elif len(p) == 1:
+			if len(s) == 1 and (p[0] == '.' or p[0] == s[0]):
+				return True
+			else:
+				return False
+
+		else:
+			if p[1] == '*':
+				if len(s) > 0 and (p[0] == '.' or p[0] == s[0]):
+					return self.isMatch(s, p[2:]) or self.isMatch(s[1:], p)
+				else:
+					return self.isMatch(s, p[2:])
+
+			elif len(s) > 0 and (p[0] == '.' or p[0] == s[0]):
+				return self.isMatch(s[1:], p[1:])
+			else:
+				return False
